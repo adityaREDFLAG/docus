0275806bbf3b41c06ba10fb5a9c64e9c6b81b75e
adityaREDFLAG <121813330+adityaREDFLAG@users.noreply.github.com>
2023-10-25 13:47:54 +0000

    yes

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   docus: {
     title: 'Zono',
     description: 'You Say We Do.',
-    image: './public/favicon.ico',
+    image: './public/favicon.png',
     socials: {
       twitter: 'Zono_Hub',
       github: 'Zono-Bot',
