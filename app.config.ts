0d5ab49f231e2dbfb6704c75fcdd31894e828e60
adityaREDFLAG <121813330+adityaREDFLAG@users.noreply.github.com>
2023-10-25 08:38:56 +0000

    Zono

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,11 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'Zono',
+    description: 'You Say We Do.',
+    image: './public/favicon.ico',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      twitter: 'Zono_Hub',
+      github: 'Zono-Bot',
     },
     github: {
       dir: '.starters/default/content',
